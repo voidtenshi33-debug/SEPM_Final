44cee841b764154bb62fe6fbbeb19c14e1ef2989
Anushka Shevate <voidtenshi33@gmail.com>
2026-03-08 08:55:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2604178974-10124",
+  "appId": "1:97379658293:web:f1b43ec3e0742f5a46df7c",
+  "apiKey": "AIzaSyApDyUDpvcYKi9ZF7zZK0fcE0VYNe4n-hk",
+  "authDomain": "studio-2604178974-10124.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "97379658293"
+};
